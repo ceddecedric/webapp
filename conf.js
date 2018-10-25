3176a919a92d3a5f4f312077b137d0076102168c
f76278 <cedric.amegan@enedis.fr>
2018-10-25 11:13:08 +0200

    detection de l environnement local

--- conf.js
+++ conf.js
@@ -2,3 +2,9 @@
 var base_url = "";
 var base_url_icons = "";
 var siteId = "";
+
+if (window.location.href.indexOf("localhost") > -1 || window.location.href.indexOf("127.0.0.1") > -1) {
+    base_url = "http://localhost/webapp/";
+    base_url_icons = "http://localhost/webapp/";
+    siteId = "local";
+}
